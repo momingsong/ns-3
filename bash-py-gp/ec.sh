80b46c9510fc1359ae0941ba468b96653b15fc97
momingsong <tonysong820@gmail.com>
2016-02-10 08:55:33 -0500

    erasure code script

--- bash-py-gp/ec.sh
+++ bash-py-gp/ec.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-block=(100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000)
-redundancy=(0.1 0.2 0.3 0.4 0.5)
+eck=(2 4 6 8 10)
+ecm=(1 2 3)
+ecmpm=(20 40 60 80 100 120)
 
-for b in ${block[@]}
+for k in ${eck[@]}
   do
-  for r in ${redundancy[@]}
+  for m in ${ecm[@]}
     do
-      ./waf --run "pec_grid_immobile  --dataAmount=50000 --redundancy=1 --tracePath=ec_b${b}_r${r} --enableRedundancyDetection=true --enableCollisionAvoidance=true --enableMultiRound=false --maxMetadataPerMessage=-1 --maxBackoff=2  --enableRetransmit=false --retransmitRetry=5 --retransmitTimeout=0.5 --enableErasureCoding=true --erasureCodingRedundancyRate=$r --erasureCodingBlockSize=$b"
+    	for mpm in ${ecmpm[@]}
+    	do
+      ./waf --run "pec_grid_immobile  --dataAmount=10000 --redundancy=1 --tracePath=ec_k${k}_m${m}_mpm${mpm} --enableRedundancyDetection=false --enableMultiRound=false --enableCollisionAvoidance=true --maxMetadataPerMessage=${mpm} --maxBackoff=0  --enableRetransmit=false --enableErasureCoding=true --erasureCodingK=${k} --erasureCodingM=${m}"
+  done
   done
 done
