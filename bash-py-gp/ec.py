c3b46ea2913c0915015c380c1aa2b7ae5c3eded2
momingsong <tonysong820@gmail.com>
2016-02-10 09:37:09 -0500

    py script

--- bash-py-gp/ec.py
+++ bash-py-gp/ec.py
@@ -132,26 +132,27 @@ def RecvToSendRatioHopnonce(typePrefix, directory):
 
 #Get recall and latency
 def RecallAndLatency(typePrefix, directory):
-    recallf = open("./%(typePrefix)s-recall.data"%vars(), "w")
-    latencyf = open("./%(typePrefix)s-latency.data"%vars(), "w")
-    recallf.write("#Data for recall of the %(typePrefix)s\n"%vars())
-    latencyf.write("#Data for latency of the %(typePrefix)s\n"%vars())
-   
-    recallf.write("#M: 1,2,3\n")
-    latencyf.write("#M: 1,2,3\n")
-    for m in [1,2,3]:
-        recallf.write(str(m)+" ")
-        latencyf.write(str(m)+" ")
-        for k in [2,4,6,8,10]:
-            file = open("%(directory)s%(typePrefix)s_k%(k)s_m%(m)s_mpm20_0.data"%vars())
-            line = file.readlines()[-1].split()
-            recallf.write(str(float(line[1])/50000)+" ")
-            latencyf.write(line[0]+" ")
-            file.close()
-        recallf.write("\n")
-        latencyf.write("\n")
-    recallf.close()
-    latencyf.close()
+    for mpm in [20,40,60,80,100,120]
+        recallf = open("./%(typePrefix)s-%(mpm)s-recall.data"%vars(), "w")
+        latencyf = open("./%(typePrefix)s-%(mpm)s-latency.data"%vars(), "w")
+        recallf.write("#Data for recall of the %(typePrefix)s\n"%vars())
+        latencyf.write("#Data for latency of the %(typePrefix)s\n"%vars())
+       
+        recallf.write("#M: 1,2,3\n")
+        latencyf.write("#M: 1,2,3\n")
+        for m in [1,2,3]:
+            recallf.write(str(m)+" ")
+            latencyf.write(str(m)+" ")
+            for k in [2,4,6,8,10]:
+                file = open("%(directory)s%(typePrefix)s_k%(k)s_m%(m)s_mpm%(mpm)s_0.data"%vars())
+                line = file.readlines()[-1].split()
+                recallf.write(str(float(line[1])/10000)+" ")
+                latencyf.write(line[0]+" ")
+                file.close()
+            recallf.write("\n")
+            latencyf.write("\n")
+        recallf.close()
+        latencyf.close()
     #os.system("gnuplot collision-avoidance-recall.gp")
     #os.system("gnuplot collision-avoidance-latency.gp")
 
