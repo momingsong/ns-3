e24fce13c42191bff0288381a9eb669f7246c812
momingsong <tonysong820@gmail.com>
2016-02-25 16:07:31 -0500

    mobility

--- multi-round-test-certain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-threshold_round=(0 0.1 0.2 0.3 0.4 0.5)
-threshold_data_discovery=(0 0.1 0.2 0.3 0.4 0.5)
-
-    #q =`echo "scale=2;$ws / 5.0" | bc` 
-for thr in ${threshold_round[@]}
-    do
-        ./waf --run "pec_grid_immobile  --dataAmount=50000 --redundancy=1 --tracePath=multi_round_thr${thr}_ws12 --enableRedundancyDetection=true --enableCollisionAvoidance=true --enableMultiRound=true --maxMetadataPerMessage=-1 --maxBackoff=2.5 --multiRoundRoundFinishThreshold=$thr --multiRoundDiscoveryFinishThreshold=0 --bloomFilterSizeMax=60000 --bloomFilterSizeMin=10000 --bloomFilterFalsePositive=0.01 --multiRoundSlotSize=0.2 --multiRoundWindowSize=12"
-done
